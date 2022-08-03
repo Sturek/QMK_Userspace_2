23841825e7345b5cd813e3a190a1a8d15154358b
Sturek <Sturek@users.noreply.github.com>
2022-08-03 23:15:58 +0200

    changed order

--- wrappers.h
+++ wrappers.h
@@ -1,5 +1,23 @@
 #pragma once
 
+// QWERTY
+#define LAYER \
+TG(1),  TG(2),    TG(3),    TG(4)
+
+// Lower
+#define WTF \
+TG(1), KC_W, KC_T, KC_F
+
+// Raise
+#define BBQ \
+TG(2), KC_B, KC_B, KC_Q
+
+// Adjust
+#define MKD \
+TG(3), KC_M, KC_K, KC_D
+
+#define LAYOUT_wrapper_ortho_1x4(...) LAYOUT_ortho_1x4(__VA_ARGS__)
+
 
 // QWERTY
 #define BASE \
@@ -30,21 +48,4 @@ _______, _______, _______, _______, _______, _______, _______, KC_INS,  KC_DEL, 
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ 
 
 #define LAYOUT_wrapper_ortho_4x12(...) LAYOUT_ortho_4x12(__VA_ARGS__)
-#define LAYOUT_wrapper_macro_1x4(...) LAYOUT_ortho_1x4(__VA_ARGS__)
-
-// QWERTY
-#define LAYER \
-TG(1),  TG(2),    TG(3),    TG(4)
-
-// Lower
-#define WTF \
-TG(1), KC_W, KC_T, KC_F
-
-// Raise
-#define BBQ \
-TG(2), KC_B, KC_B, KC_Q
-
-// Adjust
-#define MKD \
-TG(3), KC_M, KC_K, KC_D
 
